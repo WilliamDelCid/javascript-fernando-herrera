5bb1b166a1fbc14eea9b3915221ee9f26e8876d9
WilliamDelCid <dm18019@ues.edu.sv>
2023-01-06 22:28:48 -0600

    Alerts in JS

---
+++ 01-Fundamentos/assets/js/alerts.js
@@ -0,0 +1,5 @@
+// alert('Hola Mundo');
+
+// let miNombre = prompt('What*s your name');
+
+// const seleccion = confirm('Esta seguro de comer hoy?');
---
+++ 01-Fundamentos/assets/js/app.js
@@ -0,0 +1,7 @@
+let a = 10, b = 40, x = a + b;
+
+// console.table({ a, b, x });
+// console.table({ a, b, x });
+
+// console.table({ a, b, x });
+
